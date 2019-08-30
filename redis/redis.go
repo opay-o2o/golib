3637929d09cbcbab8d9523ab1cfd4e63c39aa6c2
zhao.chang <changz@opay.team>
2019-08-30 11:52:18 +0800

    add poolsize config for redis

--- redis/redis.go
+++ redis/redis.go
@@ -12,6 +12,7 @@ type Config struct {
 	Port     int    `toml:"port" json:"port"`
 	Password string `toml:"password" json:"password"`
 	Database int    `toml:"database" json:"database"`
+	PoolSize int    `toml:"poolsize" json:"poolsize"`
 }
 
 func (c *Config) GetAddr() string {
@@ -27,7 +28,7 @@ func (p *Pool) Add(name string, c *Config) {
 	p.locker.Lock()
 	defer p.locker.Unlock()
 
-	p.clients[name] = redis.NewClient(&redis.Options{Addr: c.GetAddr(), Password: c.Password, DB: c.Database})
+	p.clients[name] = redis.NewClient(&redis.Options{Addr: c.GetAddr(), Password: c.Password, DB: c.Database, PoolSize: c.PoolSize})
 }
 
 func (p *Pool) Get(name string) (*redis.Client, error) {
