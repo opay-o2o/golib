0a668d0d9dacf42930cf4af0fdd2f52cbab234cc
xiaohua.mei <xiaohua.mei@ushow.media>
2019-10-21 11:03:50 +0800

    get ip address on interface

--- net2/net.go
+++ net2/net.go
@@ -18,3 +18,26 @@ func GetLocalIp() (ip string, err error) {
 
 	return
 }
+
+func GetInterfaceIp(name string) (ip string, err error) {
+	inter, err := net.InterfaceByName(name)
+
+	if err != nil {
+		return
+	}
+
+	addrs, err := inter.Addrs()
+
+	if err != nil {
+		return
+	}
+
+	for _, address := range addrs {
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() && ipnet.IP.To4() != nil {
+			ip = ipnet.IP.String()
+			return
+		}
+	}
+
+	return
+}
