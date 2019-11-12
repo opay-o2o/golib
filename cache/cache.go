9ab04669c8f19acdeef6d2382adc8851545dc93c
xiaohua.mei <zivn@me.com>
2019-11-12 17:52:55 +0800

    Optimize cache code

--- cache/cache.go
+++ cache/cache.go
@@ -11,19 +11,20 @@ const (
 )
 
 type Item struct {
-	data   interface{}
-	expire *time.Time
+	data     interface{}
+	createAt int64
+	expire   int64
 }
 
 type Cache struct {
 	sync.RWMutex
-	items map[interface{}]*Item
+	items map[string]*Item
 	timer *time.Ticker
 }
 
 func NewCache() *Cache {
 	c := &Cache{
-		items: make(map[interface{}]*Item, initSize),
+		items: make(map[string]*Item, initSize),
 		timer: time.NewTicker(interval * time.Second),
 	}
 
@@ -31,46 +32,44 @@ func NewCache() *Cache {
 	return c
 }
 
-func (c *Cache) Get(key interface{}) (interface{}, bool) {
+func (c *Cache) Get(key string) (interface{}, bool) {
 	c.RLock()
 	defer c.RUnlock()
 
-	if item, ok := c.items[key]; ok && (item.expire == nil || time.Now().Before(*item.expire)) {
+	if item, ok := c.items[key]; ok && (item.expire == 0 || time.Now().Unix() < item.createAt+item.expire) {
 		return item.data, true
 	}
 
 	return nil, false
 }
 
-func (c *Cache) Set(key interface{}, value interface{}, expire time.Duration) {
+func (c *Cache) Set(key string, value interface{}, expire time.Duration) {
 	c.Lock()
 	defer c.Unlock()
 
-	item := &Item{data: value}
+	item := &Item{data: value, createAt: time.Now().Unix()}
 
 	if expire > 0 {
-		deadline := time.Now().Add(expire)
-		item.expire = &deadline
+		item.expire = int64(expire / time.Second)
 	}
 
 	c.items[key] = item
 }
 
-func (c *Cache) Expire(key interface{}, expire time.Duration) {
+func (c *Cache) Expire(key string, expire time.Duration) {
 	c.Lock()
 	defer c.Unlock()
 
 	if item, ok := c.items[key]; ok {
 		if expire > 0 {
-			deadline := time.Now().Add(expire)
-			item.expire = &deadline
+			item.expire = int64(expire / time.Second)
 		} else {
-			item.expire = nil
+			item.expire = 0
 		}
 	}
 }
 
-func (c *Cache) Delete(key interface{}) {
+func (c *Cache) Delete(key string) {
 	c.Lock()
 	defer c.Unlock()
 
@@ -83,13 +82,29 @@ func (c *Cache) cleanup() {
 	c.Lock()
 	defer c.Unlock()
 
-	nowTime := time.Now()
+	nowTime := time.Now().Unix()
 
 	for key, item := range c.items {
-		if item.expire != nil && nowTime.After(*item.expire) {
+		if item.expire > 0 && nowTime > item.createAt+item.expire {
 			delete(c.items, key)
 		}
 	}
+}
+
+func (c *Cache) GetAll() map[string]interface{} {
+	c.RLock()
+	defer c.RUnlock()
+
+	nowTime := time.Now().Unix()
+	items := make(map[string]interface{}, len(c.items))
+
+	for key, item := range c.items {
+		if item.expire > 0 && nowTime > item.createAt+item.expire {
+			items[key] = item.data
+		}
+	}
+
+	return items
 }
 
 func (c *Cache) run() {
