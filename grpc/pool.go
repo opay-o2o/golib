cf79bf7f10f43ed33b56b57d6920705d80a36fab
xiaohua.mei <zivn@me.com>
2019-11-21 18:42:41 +0800

    add a simple grpc pool

--- grpc/pool.go
+++ grpc/pool.go
@@ -2,279 +2,127 @@ package grpc
 
 import (
 	"context"
-	"fmt"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/connectivity"
 	"sync"
 	"time"
 )
 
-const (
-	defaultTimeout    = 100 * time.Second
-	checkReadyTimeout = 5 * time.Second
-	heartbeatInterval = 20 * time.Second
-)
-
-var (
-	errNoReady = fmt.Errorf("no ready")
-)
+type Dialer func(addr string) (*grpc.ClientConn, error)
 
 func DefaultDialer(addr string) (*grpc.ClientConn, error) {
 	return grpc.Dial(addr, grpc.WithInsecure())
 }
 
-type DialFunc func(addr string) (*grpc.ClientConn, error)
-type ReadyCheckFunc func(ctx context.Context, conn *grpc.ClientConn) connectivity.State
-
 type Pool struct {
-	sync.RWMutex
-	dial              DialFunc
-	readyCheck        ReadyCheckFunc
-	connections       map[string]*trackedConn
-	alives            map[string]*trackedConn
-	timeout           time.Duration
-	checkReadyTimeout time.Duration
-	heartbeatInterval time.Duration
-
-	ctx    context.Context
-	cannel context.CancelFunc
-}
-
-type PoolOption func(*Pool)
-
-func SetTimeout(timeout time.Duration) PoolOption {
-	return func(o *Pool) {
-		o.timeout = timeout
-	}
-}
-
-func SetCheckReadyTimeout(timeout time.Duration) PoolOption {
-	return func(o *Pool) {
-		o.checkReadyTimeout = timeout
-	}
-}
-
-func SetHeartbeatInterval(interval time.Duration) PoolOption {
-	return func(o *Pool) {
-		o.heartbeatInterval = interval
-	}
-}
-
-func CustomReadyCheck(f ReadyCheckFunc) PoolOption {
-	return func(o *Pool) {
-		o.readyCheck = f
-	}
-}
-
-func NewPool(dial DialFunc, opts ...PoolOption) *Pool {
-	ctx, cannel := context.WithCancel(context.Background())
-	ct := &Pool{
-		dial:              dial,
-		readyCheck:        defaultReadyCheck,
-		connections:       make(map[string]*trackedConn),
-		alives:            make(map[string]*trackedConn),
-		timeout:           defaultTimeout,
-		checkReadyTimeout: checkReadyTimeout,
-		heartbeatInterval: heartbeatInterval,
-
-		ctx:    ctx,
-		cannel: cannel,
-	}
-
-	for _, opt := range opts {
-		opt(ct)
-	}
-
-	return ct
-}
-
-func (ct *Pool) GetConn(addr string) (*grpc.ClientConn, error) {
-	return ct.getConn(addr, false)
-}
-
-func (ct *Pool) Dial(addr string) (*grpc.ClientConn, error) {
-	return ct.getConn(addr, true)
-}
-
-func (ct *Pool) getConn(addr string, force bool) (*grpc.ClientConn, error) {
-	ct.Lock()
-
-	tc, ok := ct.connections[addr]
-
-	if !ok {
-		tc = &trackedConn{
-			addr:    addr,
-			tracker: ct,
-		}
-		ct.connections[addr] = tc
-	}
-
-	ct.Unlock()
-
-	err := tc.tryconn(ct.ctx, force)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return tc.conn, nil
-}
-
-func (ct *Pool) connReady(tc *trackedConn) {
-	ct.Lock()
-	defer ct.Unlock()
-
-	ct.alives[tc.addr] = tc
-}
-
-func (ct *Pool) connUnReady(addr string) {
-	ct.Lock()
-	defer ct.Unlock()
-
-	delete(ct.alives, addr)
-}
-
-func (ct *Pool) Alives() []string {
-	ct.RLock()
-	defer ct.RUnlock()
-
-	alives := make([]string, 0, len(ct.alives))
-
-	for addr := range ct.alives {
-		alives = append(alives, addr)
-	}
-
-	return alives
-}
-
-type trackedConn struct {
-	sync.RWMutex
+	*sync.RWMutex
 	addr    string
-	conn    *grpc.ClientConn
-	tracker *Pool
-	state   connectivity.State
-	expires time.Time
-	retry   int
-	cannel  context.CancelFunc
+	clients chan *Connection
+	dialer  Dialer
+	idle    time.Duration
+	ttl     time.Duration
 }
 
-func (tc *trackedConn) tryconn(ctx context.Context, force bool) error {
-	tc.Lock()
-	defer tc.Unlock()
+type Connection struct {
+	conn     *grpc.ClientConn
+	pool     *Pool
+	createAt time.Time
+	lastUsed time.Time
+}
 
-	if !force && tc.conn != nil {
-		if tc.state == connectivity.Ready {
-			return nil
+func NewPool(dialer Dialer, addr string, init, capacity int, idle time.Duration, ttl ...time.Duration) (*Pool, error) {
+	if capacity <= 0 {
+		capacity = 1
+	}
+
+	if init < 0 {
+		init = 0
+	}
+
+	if init > capacity {
+		init = capacity
+	}
+
+	p := &Pool{
+		addr:    addr,
+		clients: make(chan *Connection, capacity),
+		dialer:  dialer,
+		idle:    idle,
+	}
+
+	if len(ttl) > 0 {
+		p.ttl = ttl[0]
+	}
+
+	for i := 0; i < init; i++ {
+		c, err := dialer(addr)
+
+		if err != nil {
+			return nil, err
 		}
 
-		if tc.state == connectivity.Idle {
-			return errNoReady
+		p.clients <- &Connection{
+			pool:     p,
+			conn:     c,
+			createAt: time.Now(),
+			lastUsed: time.Now(),
 		}
 	}
 
-	if tc.conn != nil {
-		tc.conn.Close()
-	}
-
-	conn, err := tc.tracker.dial(tc.addr)
-
-	if err != nil {
-		return err
-	}
-
-	tc.conn = conn
-	readyCtx, cancel := context.WithTimeout(ctx, tc.tracker.checkReadyTimeout)
-	defer cancel()
-
-	checkStatus := tc.tracker.readyCheck(readyCtx, tc.conn)
-	hbCtx, hbCancel := context.WithCancel(ctx)
-	tc.cannel = hbCancel
-
-	go tc.heartbeat(hbCtx)
-
-	if checkStatus != connectivity.Ready {
-		return errNoReady
-	}
-
-	tc.ready()
-	return nil
+	return p, nil
 }
 
-func (tc *trackedConn) getState() connectivity.State {
-	tc.RLock()
-	defer tc.RUnlock()
-	return tc.state
-}
-
-func (tc *trackedConn) healthCheck(ctx context.Context) {
-	tc.Lock()
-	defer tc.Unlock()
-
-	ctx, cancel := context.WithTimeout(ctx, tc.tracker.checkReadyTimeout)
-	defer cancel()
-
-	switch tc.tracker.readyCheck(ctx, tc.conn) {
-	case connectivity.Ready:
-		tc.ready()
-	case connectivity.Shutdown:
-		tc.shutdown()
-	case connectivity.Idle:
-		if tc.expired() {
-			tc.shutdown()
-		} else {
-			tc.idle()
-		}
-	}
-}
-
-func defaultReadyCheck(ctx context.Context, conn *grpc.ClientConn) connectivity.State {
+func (p *Pool) Get(ctx context.Context) (*Connection, error) {
 	for {
-		s := conn.GetState()
+		select {
+		case client := <-p.clients:
+			if p.idle > 0 && client.lastUsed.Add(p.idle).Before(time.Now()) {
+				_ = client.conn.Close()
+				continue
+			}
 
-		if s == connectivity.Ready || s == connectivity.Shutdown {
-			return s
-		}
+			if p.ttl > 0 && client.createAt.Add(p.ttl).Before(time.Now()) {
+				_ = client.conn.Close()
+				continue
+			}
 
-		if !conn.WaitForStateChange(ctx, s) {
-			return connectivity.Idle
+			client.lastUsed = time.Now()
+			return client, nil
+		default:
+			c, err := p.dialer(p.addr)
+
+			if err != nil {
+				return nil, err
+			}
+
+			client := &Connection{
+				pool:     p,
+				conn:     c,
+				createAt: time.Now(),
+				lastUsed: time.Now(),
+			}
+
+			return client, nil
 		}
 	}
 }
 
-func (tc *trackedConn) ready() {
-	tc.state = connectivity.Ready
-	tc.expires = time.Now().Add(tc.tracker.timeout)
-	tc.retry = 0
-	tc.tracker.connReady(tc)
-}
+func (c *Connection) Close() {
+	if c == nil || c.conn == nil {
+		return
+	}
 
-func (tc *trackedConn) idle() {
-	tc.state = connectivity.Idle
-	tc.retry++
-	tc.tracker.connUnReady(tc.addr)
-}
+	if c.pool.idle > 0 && c.lastUsed.Add(c.pool.idle).Before(time.Now()) {
+		_ = c.conn.Close()
+		return
+	}
 
-func (tc *trackedConn) shutdown() {
-	tc.state = connectivity.Shutdown
-	tc.conn.Close()
-	tc.cannel()
-	tc.tracker.connUnReady(tc.addr)
-}
+	if c.pool.ttl > 0 && c.createAt.Add(c.pool.ttl).Before(time.Now()) {
+		_ = c.conn.Close()
+		return
+	}
 
-func (tc *trackedConn) expired() bool {
-	return tc.expires.Before(time.Now())
-}
-
-func (tc *trackedConn) heartbeat(ctx context.Context) {
-	ticker := time.NewTicker(tc.tracker.heartbeatInterval)
-
-	for tc.getState() != connectivity.Shutdown {
-		select {
-		case <-ctx.Done():
-			tc.shutdown()
-			break
-		case <-ticker.C:
-			tc.healthCheck(ctx)
-		}
+	select {
+	case c.pool.clients <- c:
+	default:
 	}
 }
